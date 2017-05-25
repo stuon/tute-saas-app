64194e663e87ff145e258ebb935e10f5a680c15e
stuon <santuon@yahoo.com>
2017-05-25 21:46:27 +1000

    Added send grid to lang-tute.herokuap.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,5 +100,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'tute-saas-app.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'lang-tute.herokuapp.com', :protocol => 'https'}
 end
